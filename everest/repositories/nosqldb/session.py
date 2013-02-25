621711504edc61c44054238be8017894107a0572
F. Oliver Gathmann <fogathmann@gmail.com>
2013-02-25 08:13:58 +0100

    Incomplete - task : rewrite memory repository implementation

--- everest/repositories/nosqldb/session.py
+++ everest/repositories/nosqldb/session.py
@@ -9,6 +9,9 @@ from everest.repositories.base import SessionFactory
 from threading import local
 from transaction.interfaces import IDataManager
 from zope.interface import implements # pylint: disable=E0611,F0401
+from weakref import WeakValueDictionary
+from everest.utils import id_generator
+
 
 __docformat__ = 'reStructuredText en'
 __all__ = []
@@ -46,11 +49,173 @@ class DataManager(object):
         return "everest:%d" % id(self.session)
 
 
+class EntityCache(object):
+    """
+    Cache for entities.
+    
+    Supports add and remove operations as well as lookup by ID and 
+    by slug.
+    """
+    def __init__(self):
+        # List of cached entities. This is the only place we are holding a
+        # real reference to the entity.
+        self.__entities = []
+        # Dictionary mapping entity IDs to entities for fast lookup by ID.
+        self.__id_map = WeakValueDictionary()
+        # Dictionary mapping entity slugs to entities for fast lookup by slug.
+        self.__slug_map = WeakValueDictionary()
+
+    def get_by_id(self, entity_id):
+        """
+        Performs a lookup of an entity by its ID.
+        
+        :param int entity_id: entity ID.
+        :return: entity found or ``None``.
+        """
+        return self.__id_map.get(entity_id)
+
+    def has_id(self, entity_id):
+        return entity_id in self.__id_map
+
+    def get_by_slug(self, entity_slug):
+        """
+        Performs a lookup of an entity by its slug.
+        
+        :param str entity_id: entity slug.
+        :return: entity found or ``None``.
+        """
+        return self.__slug_map.get(entity_slug)
+
+    def has_slug(self, entity_slug):
+        return entity_slug in self.__slug_map
+
+    def add(self, entity):
+        """
+        Adds the given entity to this cache.
+        
+        :param entity: Entity to add.
+        :type entity: Object implementing :class:`everest.interfaces.IEntity`.
+        :raises ValueError: If the ID of the entity to add is ``None``.
+        """
+        if entity.id is None:
+            raise ValueError('Entity ID must not be None.')
+        self.__id_map[entity.id] = entity
+        # Unlike the ID, the slug can be a lazy attribute depending on the
+        # value of other (possibly not yet initialized) attributes which is
+        # why we can not always assume it is available at this point.
+        if not entity.slug is None:
+            self.__slug_map[entity.slug] = entity
+        self.__entities.append(entity)
+
+    def remove(self, entity):
+        """
+        Removes the given entity from this cache.
+        
+        :param entity: Entity to remove.
+        :type entity: Object implementing :class:`everest.interfaces.IEntity`.
+        :raises KeyError: If the given entity is not in this cache.
+        """
+        del self.__id_map[entity.id]
+        # We may not have the slug in the slug map because it might not have
+        # been available by the time the entity was added.
+        self.__slug_map.pop(entity.slug, None)
+        self.__entities.remove(entity)
+
+    def replace(self, entity):
+        """
+        Replaces the current entity that has the same ID as the given new
+        entity with the latter.
+        
+        :param entity: Entity to replace.
+        :type entity: Object implementing :class:`everest.interfaces.IEntity`.
+        """
+        old_entity = self.__id_map[entity.id]
+        self.remove(old_entity)
+        self.add(entity)
+
+
+class EntityIdGenerator(object):
+    """
+    Helper class generating unique sequential IDs for each entity type.
+    """
+    def __init__(self):
+        self.__id_gen_map = {}
+        self.__next_id_map = {}
+
+    def get_current_id(self, entity_class):
+        return self.__next_id_map.get(entity_class, 0)
+
+    def set_current_id(self, entity_class, entity_id):
+        id_gen = self.__get_id_generator(entity_class)
+        id_gen.send(entity_id)
+
+    def get_next_id(self, entity_class):
+        id_gen = self.__get_id_generator(entity_class)
+        next_id = id_gen.next()
+        self.__next_id_map[entity_class] = next_id
+        return next_id - 1
+
+    def __get_id_generator(self, ent_cls):
+        id_gen = self.__id_gen_map.get(ent_cls)
+        if id_gen is None:
+            id_gen = self.__id_gen_map[ent_cls] = id_generator()
+            self.__next_id_map[ent_cls] = id_gen.next()
+        return id_gen
+
+
+class EntityCacheManager(object):
+    def __init__(self):
+        self.__cache_map = {}
+
+    def __getitem__(self, entity_class):
+        cache = self.__cache_map.get(entity_class)
+        if cache is None:
+            cache = self._initialize_cache(entity_class)
+        return cache
+
+    def _initialize_cache(self, ent_cls):
+        cache = self.__cache_map[ent_cls] = EntityCache()
+        return cache
+
+
+class RepositoryEntityCacheManager(EntityCacheManager):
+    def __init__(self, repository):
+        EntityCacheManager.__init__(self)
+        self.__repository = repository
+
+    def _initialize_cache(self, ent_cls):
+        cache = EntityCacheManager._initialize_cache(self, ent_cls)
+        loader = self.__repository.configuration['loader']
+        if not loader is None:
+            max_id = -1
+            for ent in loader(ent_cls):
+                if ent.id is None:
+                    ent.id = self.__repository.get_next_id(ent_cls)
+                elif isinstance(ent.id, int) and ent.id >= max_id:
+                    # If the loaded entity already has an ID, record the
+                    # highest ID so we can adjust the ID generator.
+                    max_id = ent.id + 1
+                cache.add(ent)
+            if max_id != -1 \
+               and max_id > self.__repository.get_current_id(ent_cls):
+                self.__repository.set_current_id(ent_cls, max_id)
+        return cache
+
+
 class Session(object):
     """
+    Session object.
+    
+    The session
+     * Holds a Unit Of Work;
+     * Serves as identity and slug map;
+
+     * Manages the transaction?!
+     * Locking the repository?!
     """
     def __init__(self, unit_of_work):
         self.__unit_of_work = unit_of_work
+        self.__cache_mgr = EntityCacheManager()
 
     def commit(self):
         self.__unit_of_work.commit()
@@ -58,17 +223,50 @@ class Session(object):
     def rollback(self):
         self.__unit_of_work.rollback()
 
-    def add(self, entity_cls, entity):
-        pass
+    def add(self, entity_class, entity):
+        """
+        Adds the given entity of the given entity class to the session.
+        
+        At the point an entity is added, it must not have an ID or a slug
+        of another entity that is already in the session. However, both the ID
+        and the slug may be ``None`` values.
+        """
+        cache = self.__cache_mgr[entity_class]
+        if not entity.id is None and cache.has_id(entity.id):
+            raise ValueError('Duplicate entity key "%s".' % entity.id)
+        if not entity.slug is None and cache.has_slug(entity.slug):
+            raise ValueError('Duplicate entity slug "%s".' % entity.slug)
+        self.__unit_of_work.register_new(entity)
+        # By now, we have a unique ID, so we can add to the identity map.
+        cache.add(entity)
 
-    def remove(self, entity_cls, entity):
+    def remove(self, entity):
+        self.__unit_of_work.mark_deleted(entity)
+
+    def merge(self, entity_cls, entity):
         pass
 
     def get_by_id(self, entity_cls, entity_id):
-        pass
+        """
+        Retrieves the entity for the specified entity class and ID.
+        """
+        return self.__cache_mgr[entity_cls].get_by_id(entity_id)
 
     def get_by_slug(self, entity_cls, entity_slug):
-        pass
+        """
+        Retrieves the entity for the specified entity class and slug.
+        
+        Lookup with a slug is less efficient than with an ID, since the
+        entity might not be in the cache's slug map in which case we have 
+        to look it up in the list of pending NEW entities. 
+        """
+        found_ent = self.__cache_mgr[entity_cls].get_by_slug(entity_slug)
+        if found_ent is None:
+            for ent in self.__unit_of_work.get_new(entity_cls):
+                if ent.slug == entity_slug:
+                    found_ent = ent
+                    break
+        return found_ent
 
     def get_all(self, entity_cls):
         pass
--- everest/repositories/nosqldb/uow.py
+++ everest/repositories/nosqldb/uow.py
@@ -6,8 +6,10 @@ See LICENSE.txt for licensing, CONTRIBUTORS.txt for contributor information.
 
 Created on Jan 16, 2013.
 """
+from collections import defaultdict
 from transaction.interfaces import IDataManager
 from weakref import WeakSet
+from weakref import ref
 from zope.interface import implements # pylint: disable=E0611,F0401
 
 __docformat__ = 'reStructuredText en'
@@ -22,43 +24,175 @@ class OBJECT_STATES(object):
     DIRTY = 'DIRTY'
 
 
-class ObjectStateData(object):
-    def __init__(self):
-        self.state_hash = None
+class ObjectStateManager(object):
+    """
+    Helper object to track object state.
+    
+    Initially, an object is marked as NEW (freshly instantiated) or CLEAN
+    (freshly fetched from repository).
+    
+    Only a weak reference to the tracked object is stored to avoid circular
+    references.
+    
+    Allowed transitions are CLEAN -> DIRTY, CLEAN -> DELETED, NEW -> DIRTY, 
+    NEW -> DELETED, DIRTY -> DELETED, DIRTY -> CLEAN.
+    """
+    __allowed_transitions = ((OBJECT_STATES.NEW, OBJECT_STATES.DIRTY),
+                             (OBJECT_STATES.NEW, OBJECT_STATES.DELETED),
+                             (OBJECT_STATES.CLEAN, OBJECT_STATES.DIRTY),
+                             (OBJECT_STATES.CLEAN, OBJECT_STATES.DELETED),
+                             (OBJECT_STATES.DIRTY, OBJECT_STATES.DELETED),
+                             (OBJECT_STATES.DIRTY, OBJECT_STATES.CLEAN)
+                             )
 
-    def freeze(self, obj):
-        self.state_hash = hash(self.__get_state_string(obj))
+    def __init__(self, tracked_object, initial_state):
+        self.__obj_ref = ref(tracked_object)
+        if not initial_state in (OBJECT_STATES.NEW, OBJECT_STATES.CLEAN):
+            raise ValueError('Invalid initial state "%s".' % initial_state)
+        self.__state = initial_state
+        self.__last_state_hash = hash(self.__get_state_string())
 
-    def __get_state_string(self, obj):
-        # Concatenate all attribute name:value pairs.
+    def reset_state(self):
+        self.state = OBJECT_STATES.CLEAN
+        self.__last_state_hash = hash(self.__get_state_string())
+
+    def __get_state_string(self):
+        # Concatenate all public attribute name:value pairs.
+        obj = self.__obj_ref()
         tokens = ['%s:%s' % (k, v)
                   for k, v in obj.__dict__.iteritems()
                   if not k.startswith('_')]
         return ','.join(tokens)
 
+    def __get_state(self):
+        state = self.__state
+        if state == OBJECT_STATES.CLEAN:
+            obj_hash = hash(self.__get_state_string())
+            if obj_hash != self.__last_state_hash:
+                state = OBJECT_STATES.DIRTY
+        return state
+
+    def __set_state(self, state):
+        if not (self.__get_state(), state) in self.__allowed_transitions:
+            raise ValueError('Invalid state transition %s -> %s.'
+                             % (self.__state, state))
+        self.__state = state
+
+    state = property(__get_state, __set_state)
+
 
 class UnitOfWork(object):
-    def __init__(self):
-        self.__objects = WeakSet()
+    def __init__(self, repository):
+        self.__repository = repository
+        self.__entity_set_map = defaultdict(WeakSet)
 
-    def register_clean(self, entity):
-        entity.__everest__.state = OBJECT_STATES.CLEAN
-        self.__objects.add(entity)
+    def register_new(self, entity_class, entity):
+        """
+        Registers the given entity for the given class as NEW.
+        
+        :raises ValueError: If the given entity already holds state.
+        """
+        if hasattr(entity, '__everest__'):
+            raise ValueError('Trying to register a NEW entity that has '
+                             'already been registered!')
+        entity.__everest__ = ObjectStateManager(OBJECT_STATES.NEW)
+        if entity.id is None:
+            with self.__repository.lock:
+                entity.id = self.__repository.get_next_id(entity_class)
+        self.__entity_set_map[entity_class].add(entity)
 
-    def register_new(self, entity):
-        entity.__everest__.state = OBJECT_STATES.NEW
-        self.__objects.add(entity)
+    def mark_clean(self, entity_class, entity):
+        """
+        Marks the given entity for the given class as CLEAN.
+        
+        This is done when an entity is loaded fresh from the repository or
+        after a commit.
+        """
+        if not hasattr(entity, '__everest__'):
+            entity.__everest__ = ObjectStateManager(OBJECT_STATES.CLEAN)
+        else:
+            entity.__everest__.state = OBJECT_STATES.CLEAN
+        self.__entity_set_map[entity_class].add(entity)
 
-    def register_deleted(self, entity):
+    def mark_deleted(self, entity_class, entity):
+        """
+        Marks the given entity for the given class as DELETED.
+        
+        :raises ValueError: If the given entity does not hold state.
+        """
+        if not hasattr(entity, '__everest__'):
+            raise ValueError('Trying to mark an unregistered entity as '
+                             'DELETED!')
         entity.__everest__.state = OBJECT_STATES.DELETED
-        self.__objects.add(entity)
+        self.__entity_set_map[entity_class].add(entity)
 
-    def register_dirty(self, entity):
+    def mark_dirty(self, entity_class, entity):
+        """
+        Marks the given entity for the given class as DIRTY.
+        
+        :raises ValueError: If the given entity does not hold state.
+        """
+        if not hasattr(entity, '__everest__'):
+            raise ValueError('Trying to mark an unregistered entity as '
+                             'DIRTY!')
         entity.__everest__.state = OBJECT_STATES.DIRTY
-        self.__objects.add(entity)
+        self.__entity_set_map[entity_class].add(entity)
 
-    def flush(self):
-        pass
+    def get_clean(self, entity_class=None):
+        """
+        Returns an iterator over all CLEAN entities in this unit of work
+        (optionally restricted to entities of the given class).
+        """
+        return self.__object_iterator(OBJECT_STATES.CLEAN, entity_class)
+
+    def get_new(self, entity_class=None):
+        """
+        Returns an iterator over all NEW entities in this unit of work
+        (optionally restricted to entities of the given class).
+        """
+        return self.__object_iterator(OBJECT_STATES.NEW, entity_class)
+
+    def get_deleted(self, entity_class=None):
+        """
+        Returns an iterator over all DELETED entities in this unit of work
+        (optionally restricted to entities of the given class).
+        """
+        return self.__object_iterator(OBJECT_STATES.DELETED, entity_class)
+
+    def get_dirty(self, entity_class=None):
+        """
+        Returns an iterator over all DIRTY entities in this unit of work
+        (optionally restricted to entities of the given class).
+        """
+        return self.__object_iterator(OBJECT_STATES.DIRTY, entity_class)
+
+    def commit(self):
+        """
+        
+        """
+        with self.__repository.lock:
+            for ent_cls in self.__entity_set_map.keys():
+                for ent in self.__entity_set_map[ent_cls]:
+                    if ent.state == OBJECT_STATES.DIRTY:
+                        self.__repository.replace(ent)
+                    elif ent.state == OBJECT_STATES.NEW:
+                        self.__repository.add(ent)
+                    elif ent.state == OBJECT_STATES.DELETED:
+                        self.__repository.remove(ent)
+                    ent.__everest__.reset_state()
+
+    def rollback(self):
+        self.__entity_set_map.clear()
+
+    def __object_iterator(self, state, ent_cls):
+        if ent_cls is None:
+            keys = self.__entity_set_map.keys()
+        else:
+            keys = [ent_cls]
+        for key in keys:
+            for ent in self.__entity_set_map[key]:
+                if ent.state == state:
+                    yield ent
 
 
 class DataManager(object):
